f8880d78923c204d867cc1ead2feca04fd71686e
wujh <3130568528@qq.com>
2026-06-14 00:34:12 +0800

    创建线程

--- C++Thread/源.cpp
+++ C++Thread/源.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include <thread>
+#include <string>
 
-void printHelloWorld() {
-	std::cout << "Hello World!" << std::endl;
+void printHelloWorld(std::string msg) {
+	std::cout << msg << std::endl;
 }
 
 int main() {
 	//1.创建线程
-	std::thread thread1(printHelloWorld);
+	std::thread thread1(printHelloWorld,"Hello,Thread");
+
+	//thread1.join(); //等待线程结束
+	
+	//thread1.detach(); //分离线程，线程结束后会自动回收资源
+
+	bool isjoin = thread1.joinable(); //判断线程是否可连接
+	if (isjoin) {
+		thread1.join(); //等待线程结束
+	}
+
 	return 0;
 }
