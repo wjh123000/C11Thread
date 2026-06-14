320ec602b78cc141baaf9bf5df2263b17afaa17b
wujh <3130568528@qq.com>
2026-06-14 15:11:07 +0800

    lock_guard

--- C++Thread/源.cpp
+++ C++Thread/源.cpp
@@ -2,35 +2,23 @@
 #include<thread>
 #include<mutex>
 
-std::mutex m1,m2;
+int a = 1;
+std::mutex mtx;
 
-void func_1() {
-	for (int i = 1;i <= 50;i++) {
-		m1.lock();
-		m2.lock();
-		m1.unlock();
-		m2.unlock();
+void func() {
+	for (int i = 0;i < 10000;i++) {
+		std::lock_guard<std::mutex> lg(mtx);
+		a++;
 	}
-	
-}
-
-void func_2() {
-	for (int i = 1;i <= 50;i++) {
-		m1.lock();
-		m2.lock();
-		m1.unlock();
-		m2.unlock();
-	}
-	
 }
 
 int main() {
-	std::thread t1(func_1);
-	std::thread t2(func_2);
+	std::thread t1(func);
+	std::thread t2(func);
 	t1.join();
 	t2.join();
 
-	std::cout << "over" << std::endl;
+	std::cout << a << std::endl;
 
 	return 0;
 }
