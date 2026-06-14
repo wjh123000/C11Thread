236080b0f8deb68ed97348d79f55a77da45825e5
wujh <3130568528@qq.com>
2026-06-14 16:27:51 +0800

    unique_lock

--- C++Thread/源.cpp
+++ C++Thread/源.cpp
@@ -2,14 +2,25 @@
 #include<thread>
 #include<mutex>
 
-int a = 1;
-std::mutex mtx;
+int a = 0;
+std::timed_mutex mtx;
 
 void func() {
-	for (int i = 0;i < 10000;i++) {
-		std::lock_guard<std::mutex> lg(mtx);
-		a++;
-	}
+	for (int i = 0;i < 2;i++) {
+		//std::unique_lock<std::mutex> lg(mtx);
+
+		//std::unique_lock<std::mutex> lg(mtx, std::defer_lock);//构造函数什么都不做，加锁需要手动调用，可以自动unlock
+		//lg.lock();
+
+		std::unique_lock<std::timed_mutex> lg(mtx, std::defer_lock);
+		//lg.try_lock_for(std::chrono::seconds(5));//尝试加锁，成功返回true，失败返回false
+		if (lg.try_lock_for(std::chrono::seconds(2))) {
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+			a++;
+		}
+		
+		
+	} 
 }
 
 int main() {
